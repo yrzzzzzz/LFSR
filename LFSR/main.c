e49622d6296133cceea67408b618cfc743d653ee
youranzhang <yr994423@dal.ca>
2018-03-11 20:45:39 -0300

    Initial Commit

---
+++ LFSR/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LFSR
+//
+//  Created by youran zhang on 2018-03-11.
+//  Copyright © 2018 youran zhang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
