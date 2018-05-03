e7136c310dde4626c3fcc95bb0ca459046e2659c
youranzhang <yr994423@dal.ca>
2018-05-03 10:45:51 -0300

    Init

--- LFSR/main.c
+++ LFSR/main.c
@@ -1,15 +1,41 @@
-//
-//  main.c
-//  LFSR
-//
-//  Created by youran zhang on 2018-03-11.
-//  Copyright © 2018 youran zhang. All rights reserved.
-//
-
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+#include<stdlib.h>
+void main(){
+    
+    int len;
+    printf("please input the Length of the shift register:");
+    scanf("%d",&len);
+    int r[len];
+    int numberxor;
+    int cycles;
+    int temp;
+    printf("number:");
+    scanf("%d",&numberxor);
+    int xor[numberxor];
+    printf("please input the Tap positions:\n");
+    for(int i=0;i<numberxor;i++){
+        scanf("%d",&xor[i]);
+    }
+    printf("please input the Seed value:\n");
+    for(int i=len-1;i>=0;i--){
+        scanf("%d",&r[i]);
+    }
+    printf("please input the Number of clock cycles:\n");
+    scanf("%d",&cycles);
+    for(int i=0;i<cycles;i++){
+        temp=r[xor[numberxor-1]];
+        printf("%d ",r[len-1]);
+        for(int xxoorr=numberxor-2;xxoorr>=0;xxoorr--)
+        {
+            temp=temp^r[xor[xxoorr]];
+        }
+        for(int t=len;t>0;t--){
+            r[t]=r[t-1];
+        }
+        r[0]=temp;
+    }
+    printf("\n");
 }
+
+
+
